940af92f1dab4d1768e7880a6c3632f156404fc4
Anusha-Jagannath <anushajaganath6@gmail.com>
2021-08-26 20:34:08 +0530

    user input added

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -1,10 +1,14 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Tic-Tac-Toe game");
 		TicTacToeGame game=new TicTacToeGame();
 		game.createBoard();
+		game.playerChoice();
+		
 	}
 }
--- src/com/game/TicTacToeGame.java
+++ src/com/game/TicTacToeGame.java
@@ -1,9 +1,29 @@
 package com.game;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	private char[] board=new char[10];
 	public void createBoard() {
-		for(int i=1;i<=10;i++)
+		for(int i=1;i<10;i++)
 			board[i]=' ';
 	}
+	public void playerChoice() {
+	//ask for user inputs
+	char computerChoice;
+	Scanner sc=new Scanner(System.in);
+	System.out.print("Enter player name : ");
+	String name=sc.nextLine();
+	System.out.println("Enter player's choice X or Y");
+	char choice=sc.next().charAt(0);
+	if(choice=='X'){
+		computerChoice='O';// if user has choosen x
+	}
+	else{
+		computerChoice='X';// if user has choosen 
+	}
+	System.out.println("Computer choice is "+computerChoice);
+      sc.close();
+	}
+	
 }
